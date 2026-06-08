a20de8cafa5cb9703e9d009f7eefb217e32e94f3
Arian Huapaya <arian.huapaya@gmail.com>
2026-06-08 11:13:10 +0200

    Update __main__.py

--- plugins/sime/__main__.py
+++ plugins/sime/__main__.py
@@ -5,6 +5,7 @@ import beelib
 import beelib.beekafka
 from pythonjsonlogger import jsonlogger
 from plugins.sime.harmonizer_sime import harmonize_supplies, harmonize_timeseries, end_process
+import json
 
 logger = logging.getLogger("datadis_harmonizer")
 logger.propagate = False
@@ -35,11 +36,28 @@ def main():
         raw_records = consumer.consumer.consume(100, 10)
 
         if len(raw_records) > 0:
-            static_records = [x.value() for x in raw_records if x.topic() == conf['kafka']['static_topic']]
-            ts_records = [x.value() for x in raw_records if x.topic() == conf['kafka']['ts_topic']]
+            static_records = []
+            ts_records = []
 
-            messages = [x['data'] for x in static_records if x['kwargs']['collection_type'] != "FINAL_MESSAGE"]
-            final = [x for x in static_records if x['kwargs']['collection_type'] == "FINAL_MESSAGE"]
+            for x in raw_records:
+                if x.error() or x.value() is None:
+                    continue
+
+                try:
+                    record_dict = json.loads(x.value().decode('utf-8'))
+
+                    if x.topic() == conf['kafka']['static_topic']:
+                        static_records.append(record_dict)
+                    elif x.topic() == conf['kafka']['ts_topic']:
+                        ts_records.append(record_dict)
+                except json.JSONDecodeError as e:
+                    logger.error(f"Failed to decode malformed Kafka message: {e}")
+                    continue
+
+            # 3. Process Static Records
+            messages = [x['data'] for x in static_records if
+                        x.get('kwargs', {}).get('collection_type') != "FINAL_MESSAGE"]
+            final = [x for x in static_records if x.get('kwargs', {}).get('collection_type') == "FINAL_MESSAGE"]
 
             if messages:
                 logger.debug("Processing supplies", extra={'phase': 'HARMONIZE', 'number': len(messages)})
@@ -51,19 +69,16 @@ def main():
                 logger.debug("Dispatching final event to cleanup agent", extra={'phase': 'HARMONIZE_END'})
                 cleanup_agent()
 
-            print(ts_records)
-            print(ts_records[0])
             for record in ts_records:
-                print(record)
-                print(record.keys())
-                for r in record:
-                    print(r)
-                    print(r.keys())
-                    if "sime" not in r['kwargs']['dblist']:
-                        continue
-                    if r['kwargs']['property'] not in ["EnergyConsumptionGridElectricity"]:
-                        continue
-                    harmonize_timeseries(r['data'], r['kwargs']['freq'], r['kwargs']['property'])
+                kwargs = record.get('kwargs', {})
+                data = record.get('data', [])
+
+                if "sime" not in kwargs.get('dblist', []):
+                    continue
+                if kwargs.get('property') not in ["EnergyConsumptionGridElectricity"]:
+                    continue
+
+                harmonize_timeseries(data, kwargs.get('freq'), kwargs.get('property'))
 
 
 if __name__ == '__main__':
